c026beb8a6790f10af96ee2b91613301c044bc39
Fatih Durmaz <fatih.durmaz@outlook.com>
2025-01-18 20:19:21 +0300

    first commit

--- Package.swift
+++ Package.swift
@@ -4,21 +4,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "yolo-ios-sdk",
+    name: "YOLO",
+    platforms: [
+        .iOS(.v17),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "yolo-ios-sdk",
-            targets: ["yolo-ios-sdk"]),
+            name: "YOLO",
+            targets: ["YOLO"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "yolo-ios-sdk"),
+            name: "YOLO"),
         .testTarget(
-            name: "yolo-ios-sdkTests",
-            dependencies: ["yolo-ios-sdk"]
+            name: "YOLOTests",
+            dependencies: ["YOLO"]
         ),
     ]
 )
